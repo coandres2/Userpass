1484da023b0508d7e88cf233cc55f3455cec0df5
coandres2 <amd389@gmail.com>
2015-11-05 23:02:14 +0200

    user and pass

--- usr.cpp
+++ usr.cpp
@@ -1,25 +1,48 @@
-#include <cstdlib>
 #include <iostream>
-
+#include <string>
+#include <conio.h>
 using namespace std;
-
-int main(int argc, char *argv[])
-
+int main()
 {
-    int suma=0, cec, nrint=0;
-     char cont;
-      do
-      {
-          cout<<"Valoarea cecului este: ";
-           cin>>cec;
-             suma=suma+cec;
-              nrint++;
-                cout<<"Doriti sa introduceti un alt cec? Y/da; N/nu;";
-                 cin>>cont;
+    int act;
 
-      }
-        while(cont=='Y' || cont=='y');
-            cout<<"Ati introdus: "<<nrint<<" cecuri, in valoare de: "<<suma<<" RON"<<endl;
-            system("PAUSE");
-            return EXIT_SUCCESS;
+    cout<<"Choose action: 1. Log-in; 2. Change password;";
+    cin>>act;
+     if (act==1){
+        string pass ="";
+        char ch;
+          string usr;
+            cout<<"enter user: \n"; cin<<user;
+                if(user=='adam')
+                    {
+                    cout << "Enter pass\n";
+                    ch = _getch();
+                        while(ch != 13)
+                        {//pass ii 13
+                            pass.push_back(ch);
+                                cout << '*';
+                                ch = _getch();
+                                        }
+                                if(pass == "13")
+                                {
+                                        cout << "\Acces permis :P\n"; //urmeaza o noua secventa sau sir de secvente cu diferite actiuni
+                                }else
+                                {
+      cout << "\Accesul interzis...\n";
+   }
+    }else if
+    {
+        cout<<"Introduceti vechiul user: \n"; cin>>user;
+        cout<<"introduceti vechea paronla: \n"; cin>>pass;
+        if (pass="13" && user="adam")
+        {
+            cout<<"Introd. noul user: \n"; cin>>nusr;
+            cout<<"Introd. noua parola: \n"; cin>>npass;
+        {
+            usr=nusr;
+            pass=npass;
+        }
+        }
+    }
+    }
 }
